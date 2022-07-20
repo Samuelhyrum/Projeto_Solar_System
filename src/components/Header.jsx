82feb6a5185860b076f79b2b1621a51c255ba74a
Samuel Hyrum <samuelhymp@gmail.com>
2022-07-20 15:54:19 -0300

    Requisito 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
-import React from "react";
+import React from 'react';
 
 class Header extends React.Component {
-    render() {
-        return (
-            <header>
-            <h1>Sistema Solar</h1>
-            </header>
-        )
-    }
+  render() {
+    return (
+      <header>
+        <h1>Sistema Solar</h1>
+      </header>
+    );
+  }
 }
 export default Header;
